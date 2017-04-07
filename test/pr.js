64bd048b1bfb992d69b93c54eb5235d8a8b096cb
rogeriopvl <rogeriopvl@gmail.com>
2017-04-07 16:55:42 +0100

    test: add test for pr module

---
+++ test/pr.js
@@ -0,0 +1,31 @@
+const test = require('tape')
+const proxyquire = require('proxyquire')
+const issuesMock = require('./fixtures/issues.json')
+
+const GithubMock = function GithubMock () {}
+GithubMock.prototype.getIssues = function () {
+  return { listIssues: function () {
+    return Promise.resolve({ data: issuesMock })
+  }}
+}
+
+const getPullRequests = proxyquire('../lib/pr', {
+  'github-api': GithubMock
+})
+
+test('exports function', (t) => {
+  t.equals(typeof getPullRequests, 'function')
+  t.end()
+})
+
+test('when called correctly returns only pull requests', (t) => {
+  t.plan(1)
+  const user = ''
+  const repo = ''
+  const labels = 'needs review'
+
+  getPullRequests(user, repo, labels)
+    .then((issues) => {
+      t.equals(issues.length, 2)
+    })
+})
